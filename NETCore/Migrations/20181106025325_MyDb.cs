d5cd61cea9e0e699a91f281c23efe53e6eb653fd
ThanhTung1235 <tungltd00602@fpt.edu.vn>
2018-11-07 22:43:32 +0700

    First commit

---
+++ NETCore/Migrations/20181106025325_MyDb.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NETCore.Migrations
+{
+    public partial class MyDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    RollNumber = table.Column<string>(nullable: false),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    Gender = table.Column<int>(nullable: false),
+                    Status = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.RollNumber);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Student");
+        }
+    }
+}
---
+++ NETCore/Models/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NETCore.Models
+{
+    
+    public class Student
+    {
+        [Key]
+        public string RollNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public Gender Gender { get; set; }
+        public StudentStatus Status { get; set; }
+
+        public Student()
+        {
+            this.Gender = Gender.Male;
+            this.Status = StudentStatus.Activited;
+
+        }
+    }
+    
+    public enum Gender
+    {
+        Female = 0,
+        Male = 1,
+        Other = 2
+    }
+
+    public enum StudentStatus
+    {
+        Activited = 1,
+        UnActivited = 0
+    }
+}
